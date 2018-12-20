4dc20b41f346cd56ffb5035f105b06a6e4a7fabf
jmack-87 <jerimiah.mack@gmail.com>
2018-12-19 21:54:47 -0500

    de-final tick, make settable

--- src/main/java/com/jmack/EDSystemInfluenceBot/DateCompare.java
+++ src/main/java/com/jmack/EDSystemInfluenceBot/DateCompare.java
@@ -9,38 +9,38 @@ public class DateCompare {
 	private static Date dateTime1;
 	private static Date dateTime2;
 	private static String dateComp;
-	private static final String tick = "T16:30:00.000Z";
-	
-	
+	private static String tick = "T16:30:00.000Z";
+
+
 	/**
 	 * Given a date-time string, compare against today's server tick date-time.
 	 * @param date1
 	 * @return comparison character indicating if date >, =, < today's tick
 	 */
 	public static String compareDate(String date1) {
-		
+
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.sss'Z'");
 		SimpleDateFormat todayFormat = new SimpleDateFormat("yyyy-MM-dd");
-		
+
 		String todayDate = todayFormat.format(new Date());
 		todayDate = todayDate+tick;
-		
+
 		try {
 			dateTime1 = dateFormat.parse(date1);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		try {
 			dateTime2 = dateFormat.parse(todayDate);
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		int dateCompare = dateTime1.compareTo(dateTime2);
-		
+
 		switch (dateCompare) {
 			case 1:
 				dateComp = "+";
@@ -52,7 +52,15 @@ public class DateCompare {
 				dateComp = "-";
 				break;
 		}
-		
+
 		return dateComp;
 	}
+
+
+	public static void setTick(String time) {
+
+		DateCompare.tick = time;
+
+	}
+
 }
